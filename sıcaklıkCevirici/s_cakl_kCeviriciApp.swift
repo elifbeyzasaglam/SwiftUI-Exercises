2989a2551fa48ead19cfa10e9f6663f07ce6e94a
Elif Beyza <saglame99@gmail.com>
2023-02-26 15:35:50 +0300

    Initial Commit

---
+++ sıcaklıkCevirici/s_cakl_kCeviriciApp.swift
@@ -0,0 +1,17 @@
+//
+//  s_cakl_kCeviriciApp.swift
+//  sıcaklıkCevirici
+//
+//  Created by ELİF BEYZA SAĞLAM on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct s_cakl_kCeviriciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
